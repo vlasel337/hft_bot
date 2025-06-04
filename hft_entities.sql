2c28b1e17e1fbb3fe8c55fc1675072b6aaa7e853
vlaselindrive <vladislav.seliutin@indriver.com>
2025-06-04 17:43:12 +0300

    Добавил в features_mart_btc логику дедубликации и округления до целого timestamp

--- hft_entities.sql
+++ hft_entities.sql
@@ -241,35 +241,35 @@ create view features_mart_btc as
 select
     snapshot_timestamp,
 
-    sum(case when type = 'bid' and level = 1 then price else 0 end) as bid_1,
-    sum(case when type = 'bid' and level = 1 then size else 0 end) as size_bid_1,
+    max(case when type = 'bid' and level = 1 then price else 0 end) as bid_1,
+    max(case when type = 'bid' and level = 1 then size else 0 end) as size_bid_1,
 
-    sum(case when type = 'bid' and level = 2 then price else 0 end) as bid_2,
-    sum(case when type = 'bid' and level = 2 then size else 0 end) as size_bid_2,
+    max(case when type = 'bid' and level = 2 then price else 0 end) as bid_2,
+    max(case when type = 'bid' and level = 2 then size else 0 end) as size_bid_2,
 
-    sum(case when type = 'bid' and level = 3 then price else 0 end) as bid_3,
-    sum(case when type = 'bid' and level = 3 then size else 0 end) as size_bid_3,
+    max(case when type = 'bid' and level = 3 then price else 0 end) as bid_3,
+    max(case when type = 'bid' and level = 3 then size else 0 end) as size_bid_3,
 
-    sum(case when type = 'bid' and level = 4 then price else 0 end) as bid_4,
-    sum(case when type = 'bid' and level = 4 then size else 0 end) as size_bid_4,
+    max(case when type = 'bid' and level = 4 then price else 0 end) as bid_4,
+    max(case when type = 'bid' and level = 4 then size else 0 end) as size_bid_4,
 
-    sum(case when type = 'bid' and level = 5 then price else 0 end) as bid_5,
-    sum(case when type = 'bid' and level = 5 then size else 0 end) as size_bid_5,
+    max(case when type = 'bid' and level = 5 then price else 0 end) as bid_5,
+    max(case when type = 'bid' and level = 5 then size else 0 end) as size_bid_5,
 
-    sum(case when type = 'ask' and level = 1 then price else 0 end) as ask_1,
-    sum(case when type = 'ask' and level = 1 then size else 0 end) as size_ask_1,
+    max(case when type = 'ask' and level = 1 then price else 0 end) as ask_1,
+    max(case when type = 'ask' and level = 1 then size else 0 end) as size_ask_1,
 
-    sum(case when type = 'ask' and level = 2 then price else 0 end) as ask_2,
-    sum(case when type = 'ask' and level = 2 then size else 0 end) as size_ask_2,
+    max(case when type = 'ask' and level = 2 then price else 0 end) as ask_2,
+    max(case when type = 'ask' and level = 2 then size else 0 end) as size_ask_2,
 
-    sum(case when type = 'ask' and level = 3 then price else 0 end) as ask_3,
-    sum(case when type = 'ask' and level = 3 then size else 0 end) as size_ask_3,
+    max(case when type = 'ask' and level = 3 then price else 0 end) as ask_3,
+    max(case when type = 'ask' and level = 3 then size else 0 end) as size_ask_3,
 
-    sum(case when type = 'ask' and level = 4 then price else 0 end) as ask_4,
-    sum(case when type = 'ask' and level = 4 then size else 0 end) as size_ask_4,
+    max(case when type = 'ask' and level = 4 then price else 0 end) as ask_4,
+    max(case when type = 'ask' and level = 4 then size else 0 end) as size_ask_4,
 
-    sum(case when type = 'ask' and level = 5 then price else 0 end) as ask_5,
-    sum(case when type = 'ask' and level = 5 then size else 0 end) as size_ask_5
+    max(case when type = 'ask' and level = 5 then price else 0 end) as ask_5,
+    max(case when type = 'ask' and level = 5 then size else 0 end) as size_ask_5
 from okx_prices_btc
 group by snapshot_timestamp
 order by snapshot_timestamp desc;
@@ -280,35 +280,35 @@ create view features_mart_eth as
 select
     snapshot_timestamp,
 
-    sum(case when type = 'bid' and level = 1 then price else 0 end) as bid_1,
-    sum(case when type = 'bid' and level = 1 then size else 0 end) as size_bid_1,
+    max(case when type = 'bid' and level = 1 then price else 0 end) as bid_1,
+    max(case when type = 'bid' and level = 1 then size else 0 end) as size_bid_1,
 
-    sum(case when type = 'bid' and level = 2 then price else 0 end) as bid_2,
-    sum(case when type = 'bid' and level = 2 then size else 0 end) as size_bid_2,
+    max(case when type = 'bid' and level = 2 then price else 0 end) as bid_2,
+    max(case when type = 'bid' and level = 2 then size else 0 end) as size_bid_2,
 
-    sum(case when type = 'bid' and level = 3 then price else 0 end) as bid_3,
-    sum(case when type = 'bid' and level = 3 then size else 0 end) as size_bid_3,
+    max(case when type = 'bid' and level = 3 then price else 0 end) as bid_3,
+    max(case when type = 'bid' and level = 3 then size else 0 end) as size_bid_3,
 
-    sum(case when type = 'bid' and level = 4 then price else 0 end) as bid_4,
-    sum(case when type = 'bid' and level = 4 then size else 0 end) as size_bid_4,
+    max(case when type = 'bid' and level = 4 then price else 0 end) as bid_4,
+    max(case when type = 'bid' and level = 4 then size else 0 end) as size_bid_4,
 
-    sum(case when type = 'bid' and level = 5 then price else 0 end) as bid_5,
-    sum(case when type = 'bid' and level = 5 then size else 0 end) as size_bid_5,
+    max(case when type = 'bid' and level = 5 then price else 0 end) as bid_5,
+    max(case when type = 'bid' and level = 5 then size else 0 end) as size_bid_5,
 
-    sum(case when type = 'ask' and level = 1 then price else 0 end) as ask_1,
-    sum(case when type = 'ask' and level = 1 then size else 0 end) as size_ask_1,
+    max(case when type = 'ask' and level = 1 then price else 0 end) as ask_1,
+    max(case when type = 'ask' and level = 1 then size else 0 end) as size_ask_1,
 
-    sum(case when type = 'ask' and level = 2 then price else 0 end) as ask_2,
-    sum(case when type = 'ask' and level = 2 then size else 0 end) as size_ask_2,
+    max(case when type = 'ask' and level = 2 then price else 0 end) as ask_2,
+    max(case when type = 'ask' and level = 2 then size else 0 end) as size_ask_2,
 
-    sum(case when type = 'ask' and level = 3 then price else 0 end) as ask_3,
-    sum(case when type = 'ask' and level = 3 then size else 0 end) as size_ask_3,
+    max(case when type = 'ask' and level = 3 then price else 0 end) as ask_3,
+    max(case when type = 'ask' and level = 3 then size else 0 end) as size_ask_3,
 
-    sum(case when type = 'ask' and level = 4 then price else 0 end) as ask_4,
-    sum(case when type = 'ask' and level = 4 then size else 0 end) as size_ask_4,
+    max(case when type = 'ask' and level = 4 then price else 0 end) as ask_4,
+    max(case when type = 'ask' and level = 4 then size else 0 end) as size_ask_4,
 
-    sum(case when type = 'ask' and level = 5 then price else 0 end) as ask_5,
-    sum(case when type = 'ask' and level = 5 then size else 0 end) as size_ask_5
+    max(case when type = 'ask' and level = 5 then price else 0 end) as ask_5,
+    max(case when type = 'ask' and level = 5 then size else 0 end) as size_ask_5
 from okx_prices_eth
 group by snapshot_timestamp
 order by snapshot_timestamp desc;
@@ -319,35 +319,35 @@ create view features_mart_sol as
 select
     snapshot_timestamp,
 
-    sum(case when type = 'bid' and level = 1 then price else 0 end) as bid_1,
-    sum(case when type = 'bid' and level = 1 then size else 0 end) as size_bid_1,
+    max(case when type = 'bid' and level = 1 then price else 0 end) as bid_1,
+    max(case when type = 'bid' and level = 1 then size else 0 end) as size_bid_1,
 
-    sum(case when type = 'bid' and level = 2 then price else 0 end) as bid_2,
-    sum(case when type = 'bid' and level = 2 then size else 0 end) as size_bid_2,
+    max(case when type = 'bid' and level = 2 then price else 0 end) as bid_2,
+    max(case when type = 'bid' and level = 2 then size else 0 end) as size_bid_2,
 
-    sum(case when type = 'bid' and level = 3 then price else 0 end) as bid_3,
-    sum(case when type = 'bid' and level = 3 then size else 0 end) as size_bid_3,
+    max(case when type = 'bid' and level = 3 then price else 0 end) as bid_3,
+    max(case when type = 'bid' and level = 3 then size else 0 end) as size_bid_3,
 
-    sum(case when type = 'bid' and level = 4 then price else 0 end) as bid_4,
-    sum(case when type = 'bid' and level = 4 then size else 0 end) as size_bid_4,
+    max(case when type = 'bid' and level = 4 then price else 0 end) as bid_4,
+    max(case when type = 'bid' and level = 4 then size else 0 end) as size_bid_4,
 
-    sum(case when type = 'bid' and level = 5 then price else 0 end) as bid_5,
-    sum(case when type = 'bid' and level = 5 then size else 0 end) as size_bid_5,
+    max(case when type = 'bid' and level = 5 then price else 0 end) as bid_5,
+    max(case when type = 'bid' and level = 5 then size else 0 end) as size_bid_5,
 
-    sum(case when type = 'ask' and level = 1 then price else 0 end) as ask_1,
-    sum(case when type = 'ask' and level = 1 then size else 0 end) as size_ask_1,
+    max(case when type = 'ask' and level = 1 then price else 0 end) as ask_1,
+    max(case when type = 'ask' and level = 1 then size else 0 end) as size_ask_1,
 
-    sum(case when type = 'ask' and level = 2 then price else 0 end) as ask_2,
-    sum(case when type = 'ask' and level = 2 then size else 0 end) as size_ask_2,
+    max(case when type = 'ask' and level = 2 then price else 0 end) as ask_2,
+    max(case when type = 'ask' and level = 2 then size else 0 end) as size_ask_2,
 
-    sum(case when type = 'ask' and level = 3 then price else 0 end) as ask_3,
-    sum(case when type = 'ask' and level = 3 then size else 0 end) as size_ask_3,
+    max(case when type = 'ask' and level = 3 then price else 0 end) as ask_3,
+    max(case when type = 'ask' and level = 3 then size else 0 end) as size_ask_3,
 
-    sum(case when type = 'ask' and level = 4 then price else 0 end) as ask_4,
-    sum(case when type = 'ask' and level = 4 then size else 0 end) as size_ask_4,
+    max(case when type = 'ask' and level = 4 then price else 0 end) as ask_4,
+    max(case when type = 'ask' and level = 4 then size else 0 end) as size_ask_4,
 
-    sum(case when type = 'ask' and level = 5 then price else 0 end) as ask_5,
-    sum(case when type = 'ask' and level = 5 then size else 0 end) as size_ask_5
+    max(case when type = 'ask' and level = 5 then price else 0 end) as ask_5,
+    max(case when type = 'ask' and level = 5 then size else 0 end) as size_ask_5
 from okx_prices_sol
 group by snapshot_timestamp
 order by snapshot_timestamp desc;
@@ -355,38 +355,39 @@ order by snapshot_timestamp desc;
 -- TON
 drop view if exists features_mart_ton;
 create view features_mart_ton as
+with
 select
     snapshot_timestamp,
 
-    sum(case when type = 'bid' and level = 1 then price else 0 end) as bid_1,
-    sum(case when type = 'bid' and level = 1 then size else 0 end) as size_bid_1,
+    max(case when type = 'bid' and level = 1 then price else 0 end) as bid_1,
+    max(case when type = 'bid' and level = 1 then size else 0 end) as size_bid_1,
 
-    sum(case when type = 'bid' and level = 2 then price else 0 end) as bid_2,
-    sum(case when type = 'bid' and level = 2 then size else 0 end) as size_bid_2,
+    max(case when type = 'bid' and level = 2 then price else 0 end) as bid_2,
+    max(case when type = 'bid' and level = 2 then size else 0 end) as size_bid_2,
 
-    sum(case when type = 'bid' and level = 3 then price else 0 end) as bid_3,
-    sum(case when type = 'bid' and level = 3 then size else 0 end) as size_bid_3,
+    max(case when type = 'bid' and level = 3 then price else 0 end) as bid_3,
+    max(case when type = 'bid' and level = 3 then size else 0 end) as size_bid_3,
 
-    sum(case when type = 'bid' and level = 4 then price else 0 end) as bid_4,
-    sum(case when type = 'bid' and level = 4 then size else 0 end) as size_bid_4,
+    max(case when type = 'bid' and level = 4 then price else 0 end) as bid_4,
+    max(case when type = 'bid' and level = 4 then size else 0 end) as size_bid_4,
 
-    sum(case when type = 'bid' and level = 5 then price else 0 end) as bid_5,
-    sum(case when type = 'bid' and level = 5 then size else 0 end) as size_bid_5,
+    max(case when type = 'bid' and level = 5 then price else 0 end) as bid_5,
+    max(case when type = 'bid' and level = 5 then size else 0 end) as size_bid_5,
 
-    sum(case when type = 'ask' and level = 1 then price else 0 end) as ask_1,
-    sum(case when type = 'ask' and level = 1 then size else 0 end) as size_ask_1,
+    max(case when type = 'ask' and level = 1 then price else 0 end) as ask_1,
+    max(case when type = 'ask' and level = 1 then size else 0 end) as size_ask_1,
 
-    sum(case when type = 'ask' and level = 2 then price else 0 end) as ask_2,
-    sum(case when type = 'ask' and level = 2 then size else 0 end) as size_ask_2,
+    max(case when type = 'ask' and level = 2 then price else 0 end) as ask_2,
+    max(case when type = 'ask' and level = 2 then size else 0 end) as size_ask_2,
 
-    sum(case when type = 'ask' and level = 3 then price else 0 end) as ask_3,
-    sum(case when type = 'ask' and level = 3 then size else 0 end) as size_ask_3,
+    max(case when type = 'ask' and level = 3 then price else 0 end) as ask_3,
+    max(case when type = 'ask' and level = 3 then size else 0 end) as size_ask_3,
 
-    sum(case when type = 'ask' and level = 4 then price else 0 end) as ask_4,
-    sum(case when type = 'ask' and level = 4 then size else 0 end) as size_ask_4,
+    max(case when type = 'ask' and level = 4 then price else 0 end) as ask_4,
+    max(case when type = 'ask' and level = 4 then size else 0 end) as size_ask_4,
 
-    sum(case when type = 'ask' and level = 5 then price else 0 end) as ask_5,
-    sum(case when type = 'ask' and level = 5 then size else 0 end) as size_ask_5
+    max(case when type = 'ask' and level = 5 then price else 0 end) as ask_5,
+    max(case when type = 'ask' and level = 5 then size else 0 end) as size_ask_5
 from okx_prices_ton
 group by snapshot_timestamp
 order by snapshot_timestamp desc;
@@ -403,5 +404,86 @@ select * from features_mart_eth;
 -- Смотрим заполняемость
 select * from data_load_stats;
 
+-- Ищем разрыв во времени, связанный с переносом БД на новый хост ()
+-- 2025-05-23 09:27:59.954000 +00:00
+-- 2025-05-23 09:48:29.804000 +00:00
+select distinct snapshot_timestamp from okx_prices_btc
+order by snapshot_timestamp desc;
 
 
+-- Поиск дублей внутри таймстемпа
+with inner_query as (
+select
+    TO_CHAR(snapshot_timestamp, 'YYYY-MM-DD HH24:MI') as dttm,
+    row_number()
+    over (partition by TO_CHAR(snapshot_timestamp, 'YYYY-MM-DD HH24:MI') order by snapshot_timestamp) as rn,
+    *
+from features_mart_btc
+order by snapshot_timestamp desc
+)
+select *
+from inner_query
+where 1=1
+    and dttm = '2025-05-31 16:51'
+order by dttm desc;
+
+
+-- Логика дедубликации
+'2025-06-01 13:14:57.604000 +00:00' -- Оставляем + округляем до '2025-06-01 13:15:00'
+'2025-06-01 13:14:02.604000 +00:00' -- Дедублицируем
+
+-- Дедубликация + округление секунд в snapshot_timestamp до следующей минуты
+drop view if exists features_mart_btc;
+create view features_mart_btc as
+with inner_tab as (
+    select
+        snapshot_timestamp,
+        TO_CHAR(snapshot_timestamp, 'YYYY-MM-DD HH24:MI') as dttm,
+        row_number() over (partition by level, type, TO_CHAR(snapshot_timestamp, 'YYYY-MM-DD HH24:MI') order by snapshot_timestamp desc) as rn,
+        type,
+        level,
+        price,
+        size
+    from okx_prices_btc
+)
+select
+    CASE
+        WHEN EXTRACT(SECOND FROM snapshot_timestamp::timestamp) > 0 THEN DATE_TRUNC('minute', snapshot_timestamp::timestamp) + INTERVAL '1 minute'
+        ELSE snapshot_timestamp::timestamp
+    END AS rounded_up_st,
+    snapshot_timestamp,
+
+    max(case when type = 'bid' and level = 1 then price else 0 end) as bid_1,
+    max(case when type = 'bid' and level = 1 then size else 0 end) as size_bid_1,
+
+    max(case when type = 'bid' and level = 2 then price else 0 end) as bid_2,
+    max(case when type = 'bid' and level = 2 then size else 0 end) as size_bid_2,
+
+    max(case when type = 'bid' and level = 3 then price else 0 end) as bid_3,
+    max(case when type = 'bid' and level = 3 then size else 0 end) as size_bid_3,
+
+    max(case when type = 'bid' and level = 4 then price else 0 end) as bid_4,
+    max(case when type = 'bid' and level = 4 then size else 0 end) as size_bid_4,
+
+    max(case when type = 'bid' and level = 5 then price else 0 end) as bid_5,
+    max(case when type = 'bid' and level = 5 then size else 0 end) as size_bid_5,
+
+    max(case when type = 'ask' and level = 1 then price else 0 end) as ask_1,
+    max(case when type = 'ask' and level = 1 then size else 0 end) as size_ask_1,
+
+    max(case when type = 'ask' and level = 2 then price else 0 end) as ask_2,
+    max(case when type = 'ask' and level = 2 then size else 0 end) as size_ask_2,
+
+    max(case when type = 'ask' and level = 3 then price else 0 end) as ask_3,
+    max(case when type = 'ask' and level = 3 then size else 0 end) as size_ask_3,
+
+    max(case when type = 'ask' and level = 4 then price else 0 end) as ask_4,
+    max(case when type = 'ask' and level = 4 then size else 0 end) as size_ask_4,
+
+    max(case when type = 'ask' and level = 5 then price else 0 end) as ask_5,
+    max(case when type = 'ask' and level = 5 then size else 0 end) as size_ask_5
+from inner_tab
+where 1=1 -- and dttm in ('2025-05-20 05:31') -- 2025-05-23 09:08;
+    and rn = 1
+group by rounded_up_st, snapshot_timestamp
+order by snapshot_timestamp desc;
